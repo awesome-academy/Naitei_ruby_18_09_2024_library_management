0d760e7251b278b9c9e1e0a93fad0c516015e62f
Nguyễn Thế Vũ B <nguyen.the.vu-b@sun-asterisk.com>
2024-12-14 18:13:01 +0700

    Book detail UI + load book detail

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  scope :by_book, ->(id){where(book_id: id).order(created_at: :desc)}
+
   validates :content,
             presence: true,
             length: {maximum: Settings.comment.max_length}
